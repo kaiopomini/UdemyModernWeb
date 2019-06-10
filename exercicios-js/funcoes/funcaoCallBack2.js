1f0af062dd94e5b27615098e4519e8cb71ea0132
Kaio Pomini <kaiopomini@gmail.com>
2019-06-09 22:35:50 -0300

    callback

---
+++ exercicios-js/funcoes/funcaoCallBack2.js
@@ -0,0 +1,34 @@
+const notas = [2.5, 10, 1.2, 8.7, 3.6, 8.9, 9.7]
+
+// sem callback
+
+let notasBaixas = []
+for (let i in notas) {
+    if (notas[i] <7) {
+        notasBaixas.push(notas[i])
+    }
+}
+
+console.log(notasBaixas)
+
+// com callback
+
+notasBaixas = notas.filter(function(nota) {
+    return nota < 7
+})
+
+console.log(notasBaixas)
+
+// com callback and arrow
+notasBaixas = notas.filter(nota => nota < 7)
+
+console.log(notasBaixas)
+
+
+// com callback and arrow na const
+
+const notasMenorQue7 = nota => nota < 7 // arrow func. na const
+
+notasBaixas = notas.filter(notasMenorQue7)
+
+console.log(notasBaixas)
