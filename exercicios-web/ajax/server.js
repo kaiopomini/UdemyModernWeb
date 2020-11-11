06f8bc486d085a4e0f41b1824413707b91f47b45
Kaio Pomini <kaiopomini@gmail.com>
2020-11-10 23:24:37 -0300

    att and fixes to continue

--- exercicios-web/ajax/server.js
+++ exercicios-web/ajax/server.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const multer = require('multer')
 
 
 const app = express()
@@ -8,5 +9,28 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, '.')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
 
-app.listen(8080, () => console.log('Executando...'))
+const upload = multer({storage}).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err){
+            return res.end('Ocorreu um erro')
+        }
+        res.end('Concluído com sucesso.') 
+    })
+})
+
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
