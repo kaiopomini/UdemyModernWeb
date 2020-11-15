7b40f99fcd9055f64b696785bc423de130f117a0
Kaio Pomini <kaiopomini@gmail.com>
2020-11-14 22:18:03 -0300

    finished ajax, starting gulp chapter

---
+++ exercicios-web/gulp/basico/gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp')
+const { series, parallel } = require('gulp')
+
+const antes1 = cb => {
+  console.log('Tarefa antes 1')
+  return cb()
+}
+
+const antes2 = cb => {
+  console.log('Tarefa antes 2')
+  return cb()
+}
+
+function copiar(cb) {
+  // gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'],{allowEmpty: true})
+  gulp.src('pastaA/**/*.txt', {allowEmpty: true})
+    .pipe(gulp.dest('pastaB'))
+  return cb()
+}
+
+const fim = cb => {
+  console.log('tarefa fim!')
+  return cb()
+}
+
+module.exports.default = series(
+  parallel(antes1, antes2),
+  copiar,
+  fim
+)
